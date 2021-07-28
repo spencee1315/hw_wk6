20ebaff96e8244f100b37b9761ad0e109dff5535
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-07-27 20:10:37 -0700

    fixing syntax

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,14 @@ function initPage() {
   const nameEl = document.getElementById("city-name");
   const currentPicEl = document.getElementById("current-pic");
   const currentTempEl = document.getElementById("temperature");
-  const currentHumidityEl = document.getElementById("humidity");4
+  const currentHumidityEl = document.getElementById("humidity");
   const currentWindEl = document.getElementById("wind-speed");
   const currentUVEl = document.getElementById("UV-index");
   const historyEl = document.getElementById("history");
   let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
   console.log(searchHistory);
-  
 
-  const APIKey = "c9a9ed03a355403f4cb9a36e931c0b4a";
+  const APIKey = "6062ba2eac804d80dff372de3b8b70e8";
   
 //  When search button is clicked, read the city name typed by the user
   function getWeather(cityName) {
